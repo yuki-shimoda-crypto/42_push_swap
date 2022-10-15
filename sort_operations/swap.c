ed5e9048e13b3267ecb945cf8d1134b6f6caae6a
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-10-15 22:20:40 +0900

    make sort operations

---
+++ sort_operations/swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 21:32:02 by yshimoda          #+#    #+#             */
+/*   Updated: 2022/10/15 21:56:17 by yshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	swap(t_stack **lst)
+{
+	t_stack	*first;
+	t_stack	*second;
+
+	if (!lst || !*lst || !(*lst)->next)
+		return ;
+	first = *lst;
+	second = (*lst)-> next;
+	first->previous = second;
+	first->next = second->next;
+	second->previous = NULL;
+	second->next = first;
+}
+
+void	sa(t_stack **lst)
+{
+	swap(lst);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_stack **lst)
+{
+	swap(lst);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_stack **sa, t_stack **sb)
+{
+	swap(sa);
+	swap(sb);
+	write(1, "ss\n", 3);
+}
