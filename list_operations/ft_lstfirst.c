0a0b29351289c78a8f8367203219f53818cef795
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-10-15 17:45:32 +0900

    make lstfirst

---
+++ list_operations/ft_lstfirst.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstfirst.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 14:41:46 by yshimoda          #+#    #+#             */
+/*   Updated: 2022/10/15 17:45:12 by yshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*ft_lstfirst(t_stack *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->previous)
+		lst = lst->previous;
+	return (lst);
+}
