2075a2c68f749e8a17d8957d22fb56afbe4750ee
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-10-16 07:04:55 +0900

    make main

--- sort_operations/main.c
+++ sort_operations/main.c
@@ -6,7 +6,7 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 21:31:25 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/10/16 06:41:12 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/10/16 07:04:31 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,21 @@ int main(void)
 	lst = ft_lstnew(1);
 	lst2 = ft_lstnew(2);
 	ft_lstadd_back(&lst, lst2);
-	printf("%d\n", lst->num);
-	printf("%ld\n", lst->index);
-	printf("%p\n", lst->next);
-	printf("%p\n", lst->previous);
-	lst = lst->next;
-	printf("%d\n", lst->num);
-	printf("%ld\n", lst->index);
-	printf("%p\n", lst->next);
-	printf("%p\n", lst->previous);
+	lst = ft_lstfirst(lst);
+	ft_lstadd_front(&lst, ft_lstnew(0));
+	while (1)
+	{
+		printf("num\t\t%d\n", lst->num);
+		printf("index\t\t%ld\n", lst->index);
+		printf("lst\t\t%p\n", lst);
+		printf("previous\t%p\n", lst->previous);
+		printf("next\t\t%p\n", lst->next);
+		printf("-------------------\n");
+		if (!(lst->next))
+			break ;
+		lst = lst->next;
+	}
+	printf("%d\n", ft_lstsize(ft_lstfirst(lst)));
+	
 	return (0);
 }
